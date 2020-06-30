5d157f31dbba6a99441045b50f45f61a84aa27a2
arvindinfo2010 <arvindinfo2010@gmail.com>
2020-06-30 19:06:04 +0530

    FlatListFullExample

--- src/ChangeTextVal.js
+++ src/ChangeTextVal.js
@@ -3,12 +3,13 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   const [Text1, setText1] = useState("Welcome Home");
+  const tDate=new Date();
 
   return (
     <View style={styles.container}>
       <Text style={{ color: '#888', fontSize: 25 }}>Let know about product country name</Text>
       <TextInput >{Text1}</TextInput>
-      <Button title="Change Text" onPress={() => setText1("Hello Text Changes")} />
+      <Button title="Change Text" onPress={() => setText1("Hello Text Changes"+tDate)} />
     </View>
   );
 }
--- src/ListScreen.js
+++ src/ListScreen.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import { Text, StyleSheet ,View} from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
 const ListScreen = () => {
+    const nNum=1;
     const friends = [
-        { name: 'Rahul'},
-        { name: 'Mukesh'},
-        { name: 'Ramesh'},
-        { name: 'Suresh' }
+        { name: 'Rahul',Age:'15' },
+        { name: 'Mukesh' ,Age:'16'},
+        { name: 'Ramesh' ,Age:'17'},
+        { name: 'Suresh' ,Age:'18'},
+        { name: 'Rahul' ,Age:'19'},
+        { name: 'Mukesh' ,Age:'29'},
+        { name: 'Ramesh' ,Age:'21'},
+        { name: 'Rahul' ,Age:'22'},
+        { name: 'Mukesh' ,Age:'23'},
+        { name: 'Ramesh' ,Age:'24'},
+        { name: 'Suresh' ,Age:'25'},
+        { name: 'Rahul' ,Age:'26'},
+        { name: 'Mukesh' ,Age:'27'},
+        { name: 'Ramesh' ,Age:'28'}
     ];
 
     return (
         <FlatList
-            keyExtractor={friends=>friends.name}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            //showsVerticalScrollIndicator={false}
+            keyExtractor={friends => friends.Age}
             data={friends}
             renderItem={({ item }) => {
-                return <Text>{item.name}</Text>
+            return <Text style={styles.textStyle}>{item.name} Age-{item.Age}</Text>
             }
             } />
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50,
+        fontSize: 20,
+        marginHorizontal:20
+    }
+
+});
 
 export default ListScreen;
